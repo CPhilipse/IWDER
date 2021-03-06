a8dc7aeea39abb6e5c24912c8c9cc04af3f730c0
Clemens <clemens@machelp.nl>
2021-03-06 14:06:32 +0100

    Feature(:sparkles:): favourites filter. Smooth hover transition.

--- js/main.js
+++ js/main.js
@@ -5,34 +5,16 @@ const SANDWICH = 'sandwich';
 // Filter dishes based on checked categories.
 const filterDishes = () => {
     const elements = document.querySelectorAll("article > article");
-    const hearts = document.querySelectorAll("article > article > .hartje");
-    console.log(hearts);
+
     const checkedPizzas = document.getElementById("pizza").checked;
     const checkedPasta = document.getElementById("pasta").checked;
     const checkedSandwiches = document.getElementById("sandwiches").checked;
     const uncheckedAll = !checkedPizzas && !checkedPasta && !checkedSandwiches;
 
-    const checkedFavourites = document.getElementById("favourites").checked;
-
     for (let i = 0; i < elements.length; i++) {
         const article = document.getElementById(`card${i}`);
         // pizza | pasta | sandwich - data category
         const elementCategory = article.dataset.category;
-
-        // abstract this in its own function. - favourites.
-        const heart = document.getElementById(`hartje${i}`);
-        const topRightCircle = document.getElementsByClassName('topRightCircle')
-        const topLeftCircle = document.getElementsByClassName('topLeftCircle')
-        // const elementHeart = heart.dataset.hartje;
-        console.log(">>> H", topRightCircle);
-        if (checkedFavourites) {
-            heart.style.backgroundColor = "red";
-            topRightCircle[0].style.backgroundColor = "red";
-        }
-
-
-
-
 
         // Remove unchecked category elements
         if (!checkedPizzas || !checkedPasta || !checkedSandwiches) {
@@ -64,5 +46,61 @@ const filterDishes = () => {
                 }
             }
         }
+    }
+}
+
+const filterFavourites = () => {
+    const elements = document.querySelectorAll("article > article");
+    const checkedFavourites = document.getElementById("favourites").checked;
+
+    for (let i = 0; i < elements.length; i++) {
+        const article = document.getElementById(`card${i}`);
+
+        const heart = document.getElementById(`hartje${i}`);
+        const favouriteStatus = heart.dataset.hartje;
+
+        if (!checkedFavourites) {
+            article.style.display = "";
+        }
+
+        const isFavourite = favouriteStatus === "isFavourite" && checkedFavourites;
+        const isNotFavourite = favouriteStatus === "isNotFavourite" && checkedFavourites;
+
+        // Show the dishes that are favourite and only when favourites is checked.
+        if (isFavourite) {
+            article.style.display = "";
+        }
+
+        // Hide the dishes that are not favourite and only when favourites is checked.
+        if (isNotFavourite) {
+            article.style.display = "none";
+        }
+    }
+}
+
+const makeFavourite = (id) => {
+    const element = document.getElementById(id);
+    const favouriteStatus = element.dataset.hartje;
+
+    // getElementsByClassName returns collection
+    const topRightCircle = document.getElementsByClassName('topRightCircle');
+    const topLeftCircle = document.getElementsByClassName('topLeftCircle');
+
+    // Get last char of id which makes each element unique
+    const lastCharOfId = id.slice(-1);
+
+    // If element is favourite, than make it not the favourite. Else make it favourite.
+    if(favouriteStatus === 'isFavourite') {
+        element.dataset.hartje = 'isNotFavourite';
+
+        element.style.backgroundColor = "antiquewhite";
+        topRightCircle[lastCharOfId].style.backgroundColor = "antiquewhite";
+        topLeftCircle[lastCharOfId].style.backgroundColor = "antiquewhite";
+    } else {
+        element.dataset.hartje = 'isFavourite';
+
+        element.style.backgroundColor = "red";
+        topRightCircle[lastCharOfId].style.backgroundColor = "red";
+        topLeftCircle[lastCharOfId].style.backgroundColor = "red";
     }
 }
